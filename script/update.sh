8f4cfc414c74e27b062da174ddbb00913d360819
Seunggeun Cho <git@sgncho.com>
2025-10-24 00:05:55 +0900

    feat: add update logic

---
+++ script/update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+has_local() {
+    local _has_local
+}
+
+has_local 2>/dev/null || alias local=typeset
+
+err() {
+    local red
+    local reset
+    red=$(tput setaf 1 2>/dev/null || echo '')
+    reset=$(tput sgr0 2>/dev/null || echo '')
+    echo "${red}error${reset}: $1" >&2
+    exit 1
+}
+
+if ! git pull origin main; then
+    err "Failed to pull from origin/main"
+fi
+
+DOTFILE_DIR="$HOME/dotfile"
+
+cd "$DOTFILE_DIR"
+ansible-playbook init.yml
